ee195d33faef627dca50a318ef4ff8dbaeec570d
salva <salva@trestristestigres.com>
2018-04-16 20:55:12 +0200

    PlayerController eje x

---
+++ navecitas/Assets/sprites/scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 3f;
+	public float maxSpeed = 5f;
+	private Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+
+		rigidbody2D.AddForce(Vector2.right * speed * h);
+
+		if(rigidbody2D.velocity.x > maxSpeed) {
+			rigidbody2D.velocity = new Vector2(maxSpeed, rigidbody2D.velocity.y);
+		}
+
+		if(rigidbody2D.velocity.x < -maxSpeed) {
+			rigidbody2D.velocity = new Vector2(-maxSpeed, rigidbody2D.velocity.y);
+		}
+
+		Debug.Log(rigidbody2D.velocity.x);
+	}
+
+	
+}
